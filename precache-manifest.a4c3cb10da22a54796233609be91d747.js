c907ec705462b762daa30d2e938b30b7c00bb8ec
yulia tikhonova <yuliatikhonova@outlook.com>
2020-08-15 23:20:05 -0700

    Updates

---
+++ precache-manifest.a4c3cb10da22a54796233609be91d747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5536d3ad3e1bde528987add0ce337e6",
+    "url": "/HW-19-Employee-Directory/index.html"
+  },
+  {
+    "revision": "dddc58be2fd5c74b0c67",
+    "url": "/HW-19-Employee-Directory/static/css/main.c1c8415d.chunk.css"
+  },
+  {
+    "revision": "03e572ac86a60c5cfb5d",
+    "url": "/HW-19-Employee-Directory/static/js/2.db1420a6.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/HW-19-Employee-Directory/static/js/2.db1420a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dddc58be2fd5c74b0c67",
+    "url": "/HW-19-Employee-Directory/static/js/main.5186701b.chunk.js"
+  },
+  {
+    "revision": "45758dcd2d6a38220b48",
+    "url": "/HW-19-Employee-Directory/static/js/runtime-main.665871bf.js"
+  }
+]);
