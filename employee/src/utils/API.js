9f65336833b176efe3a68608257813212218fc72
Yulia TIkhonova <yuliatikhonova@outlook.com>
2020-08-15 22:00:51 -0700

    Update API.js

--- employee/src/utils/API.js
+++ employee/src/utils/API.js
@@ -5,10 +5,3 @@ const BASEURL = "https://randomuser.me/api/?results=100&nat=us";
 export default function getInfo(query) {
   return axios.get(BASEURL);
 };
-
-// import axios from "axios";
-
-// export default function getEmployeeName(query) {
-//     const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
-//     return axios.get(BASEURL);
-// };
